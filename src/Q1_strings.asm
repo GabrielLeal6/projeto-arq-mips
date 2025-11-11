7c805e5db7943283749795ca008e86d7c7b58cc4
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 <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>
2025-11-11 16:45:58 -0300

    Adicionados comentários extras nas funções da Q1

--- src/Q1_strings.asm
+++ src/Q1_strings.asm
@@ -3,9 +3,12 @@
 # Arquivo referente a criação das funções pedidas na questão 1 da seção de de exercícios do projeto
 # Define as funções strcpy, memcpy, strcmp, strncmp e strcat
 
+
+# Função que cópia uma string para outro endereço
 strcpy: # entrada da função strcpy
 	move $v0, $a0 # retorna o endereço de destino para o registrador $v0
 
+# loop princiapl que faz a cópia
 loop_strcpy: # label de loop da função strcpy
 	lb $t0, 0($a1) # carrega o byte do endereço de origem para $t0
 	sb $t0, 0($a0) # salva o byte de $t0 para o endereço de destino $a0
@@ -19,11 +22,12 @@ fim_strcpy: # ponto de saída da função strcpy
 	jr $ra # retorna para o código na seção que invocou a função strcpy
 
 
-
+# função que cópia uma número de bytes (num) de um bloco para outro na memória
 memcpy: # entrada da função memcpy
 	move $v0, $a0 # retorna o endereço de destino para o registrador $v0
 	add $t1, $zero, $a2 # cria em $t1 um contador com o valor de num ($a2)
-	
+
+# loop principal que faz a cópia
 loop_memcpy: # label de loop da função memcpy
 	beqz $t1, fim_memcpy # verifica se é o fim do número de repetições do loop
 	lb $t0, 0($a1) # carrega o byte do endereço de origem para o $t0
@@ -39,9 +43,10 @@ fim_memcpy: # ponto de saída da função memcpy
 	jr $ra # retorna para o código na seção que invocou a função memcpy
 
 
-
+# função que comapara duas strings com diferentes retornos dependendo do resultado da comparação, podendo ser negativo, positivo ou zero
 strcmp: # ponto de entrada da função strcmp
-	
+
+# loop principal da comparação	
 loop_strcmp: # label de loop da função strcmp
 	lb $t0, 0($a0) # carrega o byte do endereço de origem em str1
 	lb $t1, 0($a1) # carrega o byte do endereço de origem em str2
@@ -68,9 +73,11 @@ strcmp_inteiro_positivo: # resultado caso o primeiro caractere diferente seja ma
 	jr $ra # retorna para o código na seção que invocou a função strcmp
 	
 
+# função que compara duas strings até no máximo um número (num) de bytes
 strncmp: # ponto de entrada da função strncmp
 	add $t0, $zero, $a3 # cria o contador de num ($a3) em $t0
 
+# loop principal que faz a comparação entre as strings
 loop_strncmp: # label de loop da função strncmp
 	beqz $t0, strncmp_zero # verifica se é o fim do número de repetições do loop
 	lb $t1, 0($a0) # carrega o byte do endereço de origem em str1
@@ -101,12 +108,13 @@ strncmp_inteiro_positivo:
 	jr $ra # retorna para o código na seção que invocou a função strcmp
 
 
-
+# função que concatena duas strings
 strcat: # ponto de entrada da função strcat
 	move $v0, $a0 # retorna o endereço de destino para o registrador $v0
 	add $t0, $a0, $zero # cria um iterador para chegar até a parada da string destino
 
-loop_strcat_iterate: # label do loop de iteração da string de destino para adquirir o endereço do fim da string
+# loop inicial que busca o endereço do fim da string de destino
+loop_strcat_iterate: # label do loop de iteração inicial
 	lb $t1, 0($t0) # carrega o valor da string destino na posição apontada pelo iterador $t0
 	beqz $t1, strcat_concat # pula para o a função de concatenação se encontrar a parada
 	addi $t0, $t0, 1 # move o ponteiro do iterador para o próximo caractere
@@ -115,7 +123,8 @@ loop_strcat_iterate: # label do loop de iteração da string de destino para adq
 strcat_concat: # ponto de entrada para o loop de concatenção da função strcat
 	add $t2, $a1, $zero # cria um segundo iterador para copiar os conteúdos da string source para a destino
 
-loop_strcat_concat: # label do loop da parte de concatenção da função strcat
+# loop que faz a concatenação em si, copiando os caracteres da string source para a string destino
+loop_strcat_concat: # label do loop de concatenação
 	lb $t1, 0($t2) # carrega o valor da string source apontada pelo iterador $t2
 	sb $t1, 0($t0) # salva o valor salvo em $t1 para o endereço apontado pelo iterador $t0
 	
