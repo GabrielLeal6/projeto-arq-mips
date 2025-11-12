bca7c044ad9a1fb2131f9dbe99697787a3467e8a
Valadyne <caio.marinho@ufrpe.br>
2025-11-12 20:05:37 -0300

    Criação dos comandos
    - Criados os arquivos referentes aos comandos pedidos na descrição do projeto
    - Pequena correção ortográfica no io_util

--- src/io_utils.asm
+++ src/io_utils.asm
@@ -1,6 +1,6 @@
 # Grupo composto pelos alunos Gabriel Leal de Queiroz e Caio Vinicius Marinho
 # Atividade da primeira VA de 2025.2 de Arquitetura e Organização de Computadores
-# Arquivo referente a gestão do MMIO par o projeto principal
+# Arquivo referente a gestão do MMIO para o projeto principal
 
 
 .data
