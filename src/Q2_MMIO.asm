bd73250de8b688e8b7f7969b16f2674bfa28d42b
Gabriel Leal <2006gabrielleal@gmail.com>
2025-11-12 12:17:38 -0300

    feita a questão 2 (MMIO)

---
+++ src/Q2_MMIO.asm
@@ -0,0 +1,26 @@
+# Grupo composto pelos alunos Gabriel Leal de Queiroz e Caio Vinicius Marinho
+# Atividade da primeira VA de 2025.2 de Arquitetura e Organização de Computadores
+# Arquivo referente à questão 2 da seção de exercícios
+# Implementa um "Echo" de terminal usando MMIO (Memory-Mapped I/O)
+
+.text
+.global main
+main:
+	lui $t0, 0xffff # Carrega o endereço base 0xffff0000
+
+	loop_ler: # entrada do loop de leitura do teclado
+		lw $t1, 0($t0) # carrega o status do teclado
+		andi $t1, $t1, 1 # Isola o bit "pronto"
+		beqz $t1, loop_ler # Se não estiver pronto, volta para o começo do loop
+		
+		lw $t2, 4($t0) # carrega o caractere pronto em $t2
+	
+	
+	loop_display: # entrada da parte de display
+		lw $t3, 8($t0) # carrega o status do display
+		andi $t3, $t3, 1 # Isola o bit "pronto"
+		beqz $t3, loop_display # Se não estiver pronto, volta para o começo do loop
+		sw $t2, 12($t0) # salva o caractere em $t2 para o display
+		
+		j loop_ler # volta para o começo do loop maior
+		 
