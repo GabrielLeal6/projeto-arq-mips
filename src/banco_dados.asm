cf2388cd81c99278077dd4c6bb26af60ecb1ed5a
Gabriel Leal <2006gabrielleal@gmail.com>
2025-11-12 15:35:51 -0300

    Feito a implementação inicial do MMIO para o projeto principal e a definição dos tamanhos dos dados necessários

---
+++ src/banco_dados.asm
@@ -0,0 +1,21 @@
+# Grupo composto pelos alunos Gabriel Leal de Queiroz e Caio Vinicius Marinho
+# Atividade da primeira VA de 2025.2 de Arquitetura e Organização de Computadores
+# Arquivo referente a definição dos tamanhos dos dados usados
+
+.data
+	# atributos do cliente:
+	.eqv SIZE_NOME 64
+	.eqv SIZE_CPF 12
+	.eqv SIZE_CONTA 8
+	.eqv SIZE_SALDO 4
+	.eqv SIZE_LIMITE 4
+	.eqv SIZE_DIVIDA 4
+	.eqv SIZE_FIXOS 96 # SIZE_NOME + SIZE_CPF + SIZE_CONTA + SIZE_SALDO + SIZE_LIMITE + SIZE_DIVIDA
+	
+	.eqv SIZE_TRANS 32	
+	.eqv SIZE_TRANS_TOTAL 3200 # SIZE_TRANS * 100
+	
+	.eqv SIZE_CLIENTE SIZE_FIXOS + SIZE_TRANS_TOTAL #  96 + 3200 = 3296
+	
+	banco_clientes: .space 164800 # 50 * SIZE_CLIENTE
+	
---
+++ src/io_utils.asm
@@ -0,0 +1,40 @@
+# Grupo composto pelos alunos Gabriel Leal de Queiroz e Caio Vinicius Marinho
+# Atividade da primeira VA de 2025.2 de Arquitetura e Organização de Computadores
+# Arquivo referente a gestão do MMIO par o projeto principal
+
+
+.data
+	.eqv ASCII_NL 10
+	.eqv ASCII_NULL 0
+
+.text
+
+# função que trata do MMIO
+read_line_mmio: # ponto de entrada do funcionamento do MMIO
+	lui $t0, 0xffff # Carrega o endereço base 0xffff0000
+
+loop_ler: # entrada do loop de leitura do teclado
+	lw $t1, 0($t0) # carrega o status do teclado
+	andi $t1, $t1, 1 # Isola o bit "pronto"
+	beqz $t1, loop_ler # Se não estiver pronto, volta para o começo do loop
+
+	lw $t2, 4($t0) # carrega o caractere pronto em $t2
+
+loop_display: # entrada da parte de display
+	lw $t3, 8($t0) # carrega o status do display
+	andi $t3, $t3, 1 # Isola o bit "pronto"
+	beqz $t3, loop_display # Se não estiver pronto, volta para o começo do loop
+	sw $t2, 12($t0) # salva o caractere em $t2 para o display
+
+	addi $a0, $a0, 1 # avança o ponteiro do buffer
+
+	li $t3, ASCII_NL # carrega o valor de escape (10)
+	beq $t2, $t3, fim_read_line # se a tecla pressionada vai para o fim
+
+	j loop_ler # se não foi enter, continua o loop
+
+fim_read_line: # saída do MMIO para 
+	li $t2, ASCII_NULL # Carrega o valor nulo (0)
+	sb $t2, 0($a0) # salva o sinal de parada (\0) no final da string
+
+	jr $ra # retorna para a parte do código para onde foi invocado
