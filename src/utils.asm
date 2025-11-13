42adb73a2833bc2229739c33581081be13998d07
Valadyne <caio.marinho@ufrpe.br>
2025-11-12 22:12:25 -0300

    Criação do utils
    - Criação emergencial do utils.asm para lidar com necessidades futuras
    - ínfima alteração no conta cadastrar
    - início da implementação do conta_format

---
+++ src/utils.asm
@@ -0,0 +1,4 @@
+# Grupo composto pelos alunos Gabriel Leal de Queiroz e Caio Vinicius Marinho
+# Atividade da primeira VA de 2025.2 de Arquitetura e Organização de Computadores
+# Arquivo referente a funções gerais que serão usadas no projeto
+
